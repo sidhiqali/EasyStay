4bdccfd23b72cf8fdaa7c88902b35acfeaac4513
Sidhiq Ali <sidhiqsidhiqali@gmail.com>
2023-07-16 00:38:32 +0530

    register model

---
+++ src/components/Button.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import { IconType } from 'react-icons';
+
+interface ButtonProps {
+  label: string;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+  outline?: boolean;
+  small?: boolean;
+  icon?: IconType;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  label,
+  onClick,
+  disabled,
+  outline,
+  small,
+  icon: Icon,
+}) => {
+  return (
+    <button
+      disabled={disabled}
+      onClick={onClick}
+      className={`
+        relative
+        disabled:opacity-70
+        disabled:cursor-not-allowed
+        rounded-lg
+        hover:opacity-80
+        transition
+        w-full
+        ${
+          outline
+            ? 'bg-white border-black text-black'
+            : 'bg-rose-500 border-rose-500 text-white'
+        }
+        ${
+          small
+            ? 'text-sm py-1 border-[1px] font-light'
+            : 'py-3 text-md font-semibold border-2'
+        }
+
+      `}
+    >
+      {Icon && (
+        <Icon
+          size={24}
+          className='
+            absolute
+            left-4
+            top-3
+          '
+        />
+      )}
+      {label}
+    </button>
+  );
+};
+
+export default Button;
--- src/components/navbar/UserMenu.tsx
+++ src/components/navbar/UserMenu.tsx
@@ -3,10 +3,12 @@ import { useCallback, useState } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
 import Avatar from '../Avatar';
 import MenuItem from './MenuItem';
+import useRegisterModel from '@/app/Hooks/useRegisterModel';
+
 
 const UserMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
-
+const registerModel = useRegisterModel()
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
   }, []);
@@ -35,7 +37,7 @@ const UserMenu = () => {
         <div className='absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm'>
           <div className='flex flex-col cursor-pointer'>
             <MenuItem onclick={() => {}} label='Login' />
-            <MenuItem onclick={() => {}} label='Signup' />
+            <MenuItem onclick={registerModel.onOpen} label='Signup' />
           </div>
         </div>
       )}
